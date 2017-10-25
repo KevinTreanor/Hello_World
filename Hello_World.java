053cb20f39efa18f3e10bdb20d1df1163f0df021
KevinTreanor <c15337401@mydit.ie>
2017-10-26 00:04:07 +0100

    Add files via upload

---
+++ Hello_World.java
@@ -0,0 +1,11 @@
+/*Hello world in java to be used by heroku
+25/10/2017
+Kevin Treanor */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
